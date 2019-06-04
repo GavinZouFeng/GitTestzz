4e68787ae19ccf5dcdda811748337502afcf612b
zll <963751518@qq.com>
2019-06-04 21:54:51 +0800

    修改

--- Test/src/test/GitTest.java
+++ Test/src/test/GitTest.java
@@ -12,6 +12,6 @@ public class GitTest {
         System.out.println("git第一次测试......邹峰");
         System.out.println("git第2次测试......邹峰");
         System.out.println("git第3次测试......邹峰");
-        System.out.println("aweawaeaw强制推送");
+        System.out.println("aweawaeaw强制123推送");
     }
 }
